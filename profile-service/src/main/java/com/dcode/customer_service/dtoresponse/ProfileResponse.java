df56c9896c10fb98283878ef2735a0f6dc9191ab
DuyHD75 <99957603+DuyHD75@users.noreply.github.com>
2024-11-22 20:15:18 +0700

    #11 - DuyHD75 - Implement chatbox socket

--- profile-service/src/main/java/com/dcode/customer_service/dtoresponse/ProfileResponse.java
+++
@@ -1,9 +0,0 @@
-package com.dcode.customer_service.dtoresponse;
-
-
-import com.dcode.customer_service.entity.Address;
-
-public record ProfileResponse(String id,
-                              String accumulatedPoints,
-                              Address address) {
-}
--- profile-service/src/main/java/com/dcode/customer_service/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dcode.customer_service.repository;
-
-
-import com.dcode.customer_service.entity.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-}
